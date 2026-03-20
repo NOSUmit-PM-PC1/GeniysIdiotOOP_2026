2d8bad2cc70b7bcf9af2e432ba3e6b4bb4ade37d
Мария МД <mahadrum@yandex.ru>
2026-03-20 10:08:56 +0300

    Добавили класс вопроса

---
+++ GeniusIdiot/Question.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeniusIdiot
+{
+    class Question
+    {
+        string text;
+        int answer;
+
+        public Question(string txt, int ans )
+        {
+            this.text = txt;
+            this.answer = ans;
+        }
+
+        public string ShowQuestion()
+        { 
+            return text;
+        }
+
+        public bool CheckAnswer(int userAnswer)
+        { 
+            return answer == userAnswer;
+        }
+
+    }
+}
